787fbd33951a4dba8900c9bb9de871c0469e17b3
ryaneggz <kre8mymedia@gmail.com>
2025-08-30 01:22:46 -0600

    works with tools and mcp tools

--- agents/xml_agent.py
+++ agents/xml_agent.py
@@ -1,10 +1,13 @@
 import json
 import random
+import asyncio
 from typing import Annotated, TypedDict
 from langchain_core.tools import tool
+from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_core.runnables.config import RunnableConfig
 from langgraph.types import interrupt
 from langgraph.graph import StateGraph, START
+from langchain_core.tools import BaseTool
 from langchain.chat_models import init_chat_model
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
@@ -66,31 +69,44 @@ def human_assistance(query: str) -> str:
 	human_response = interrupt({"query": query})
 	return human_response["data"]
 
-TOOLS = [get_weather, get_stock_price, human_assistance, web_scrape]
+TOOLS = [get_weather, get_stock_price, human_assistance]
 
 ###########################################
 ## Graph
 ###########################################
-class AgentState(TypedDict):
-	messages: Annotated[list[BaseMessage], add_messages]
+def create_enso_graph(tools: list[BaseTool], mcp: dict = {}):
+	mcp_client = MultiServerMCPClient(mcp)
+	mcp_tools = asyncio.run(mcp_client.get_tools())
+	combined_tools = tools + mcp_tools
+	
+	class AgentState(TypedDict):
+		messages: Annotated[list[BaseMessage], add_messages]
 
-workflow = StateGraph(AgentState)
-def agent(state: AgentState, config: RunnableConfig):
-	prompt_str = input_parser(state["messages"])
-	model = init_chat_model(
-     	model=config.get("configurable", {}).get("model", "openai:gpt-5-nano"),
-		temperature=config.get("configurable", {}).get("temperature", 0.7),
-		timeout=config.get("configurable", {}).get("timeout", 30),
-	).bind_tools(TOOLS)
-	response = model.invoke([
-		SystemMessage(content=config.get("configurable", {}).get("system", Prompts.BASIC_SYSTEM_PROMPT)),
-		HumanMessage(content=prompt_str)
-	])
-	return {"messages": [response]}
+	workflow = StateGraph(AgentState)
+	def agent(state: AgentState, config: RunnableConfig):
+		prompt_str = input_parser(state["messages"])
+		model = init_chat_model(
+			model=config.get("configurable", {}).get("model", "openai:gpt-5-nano"),
+			temperature=config.get("configurable", {}).get("temperature", 0.7),
+			timeout=config.get("configurable", {}).get("timeout", 30),
+		).bind_tools(combined_tools)
+		response = model.invoke([
+			SystemMessage(content=config.get("configurable", {}).get("system", Prompts.BASIC_SYSTEM_PROMPT)),
+			HumanMessage(content=prompt_str)
+		])
+		return {"messages": [response]}
 
-workflow.add_node("agent", agent)
-workflow.add_node("tools", ToolNode(tools=TOOLS))
-workflow.add_conditional_edges("agent", tools_condition)
-workflow.add_edge("tools", "agent")
-workflow.add_edge(START, "agent")
-workflow.compile(name="xml_agent", debug=True)
+	workflow.add_node("agent", agent)
+	workflow.add_node("tools", ToolNode(tools=combined_tools))
+	workflow.add_conditional_edges("agent", tools_condition)
+	workflow.add_edge("tools", "agent")
+	workflow.add_edge(START, "agent")
+	workflow.compile(name="xml_agent", debug=True)
+	return workflow
+
+graph = create_enso_graph(TOOLS, mcp={
+	"web_scrape": {
+		"url": "http://localhost:8050/mcp",
+		"transport": "streamable_http",
+	}
+})
